d10bb3e6e6fe5e49f6d36b269262b6a4db4b18da
MichaelVilvoorde <60927311+MichaelVilvoorde@users.noreply.github.com>
2020-09-02 12:00:08 +0200

    Github Setup
    First upload on Github

---
+++ js/scripts.js
@@ -0,0 +1,70 @@
+
+// strict function
+// (function(){
+//     "use strict";
+  
+//     Define your library...
+//   })();
+
+
+
+    (function ($) {
+    "use strict"; // strict mode
+
+
+    
+
+    // Scrollspy - adds active class on scroll to navbar-item 
+    $("body").scrollspy({
+        target: "#mainNav",
+        offset: 100
+    });
+
+    // Close hamburger menu on click
+    $(".close-collapsed-nav-on-click").click(function () {
+        $(".navbar-collapse").collapse("hide");
+    });
+
+
+
+
+
+    // Special desktop navbar on scroll
+    var navbarCollapse = function () {
+        // after ...(amount) show special nav
+        if ($("#mainNav").offset().top > 100) {
+            $("#mainNav").addClass("navbar-shrink");
+        } 
+        else {
+            $("#mainNav").removeClass("navbar-shrink");
+        }
+    };
+    $(window).scroll(navbarCollapse);
+
+    
+    
+    // Get the current year for the copyright
+    $('#year').text(new Date().getFullYear());
+    
+    
+    // Gallery
+    $(".gallery").magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        gallery:{
+          enabled: true
+        }
+      });
+
+
+    
+
+
+
+
+
+
+
+})(jQuery); // End of use strict
+
+
